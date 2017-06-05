fc0b3182f23e7044b12e2b886bdb92b29da90a25
gpdoud <gpdoud@gmail.com>
2017-06-05 16:05:10 -0400

    inital commit of source

---
+++ StudentProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProject {
+
+    class Program {
+
+        string PromptUserInput(string prompt) {
+            Console.Write(prompt);
+            string response = Console.ReadLine();
+            return response;
+        }
+
+        void Run() {
+            Student stud1 = new Student();
+
+            stud1.SetFirstName(PromptUserInput("Enter First Name: "));
+            string lname = PromptUserInput("Enter Last Name: ");
+            stud1.SetLastName(lname);
+            string tempEmail = PromptUserInput("Enter Email: ");
+            stud1.SetEmail(tempEmail);
+            string tempPhone = PromptUserInput("Enter Phone: ");
+            stud1.SetPhone(tempPhone);
+            stud1.Display();
+
+            Student studGeorge = new Student("George", "Smith");
+            //studGeorge.Display();
+            Student studAlex = new Student("Alex", "Cobb", "alex@aol.com", "513-555-1212");
+            //studAlex.Display();
+            studAlex.Print();
+            string personalData = studAlex.Print();
+            //Console.WriteLine(personalData);
+
+            //User enters data
+            //Console.Write("Enter some data: ");
+            //string response = Console.ReadLine();
+            //Console.WriteLine(response);
+            //Console.ReadKey();
+        }
+
+        static void Main(string[] args) {
+            (new Program()).Run();
+        }
+    }
+}
---
+++ StudentProject/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProject {
+
+    public class Student {
+
+        // default constructor - auto created by VS.NET if no other constructors exist
+        public Student() {
+            firstName = "George";
+            lastName = "Smith";
+        }
+        public Student(string firstName, string lastName) 
+            : this(firstName, lastName, null, null) {
+        }
+        public Student(string firstName, string lastName, string email, string phone) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string phone;
+
+        public string GetFirstName() {
+            return firstName;
+        }
+        public void SetFirstName(string firstName) {
+            this.firstName = firstName;
+        }
+        public string GetLastName() {
+            return lastName;
+        }
+        public void SetLastName(string lastName) {
+            this.lastName = lastName;
+        }
+        public string GetFullName() {
+            return GetFirstName() + " " + GetLastName();
+        }
+        public string GetEmail() {
+            return email;
+        }
+        public void SetEmail(string email) {
+            this.email = email;
+        }
+        public string GetPhone() {
+            return phone;
+        }
+        public void SetPhone(string phone) {
+            this.phone = phone;
+        }
+
+        public void Display() {
+            Console.WriteLine(GetFullName());
+            Console.WriteLine(GetEmail());
+            Console.WriteLine(GetPhone());
+            Console.WriteLine("...press any key to continue");
+            Console.ReadKey();
+        }
+
+        public string Print() {
+            return firstName + " " + lastName + "|" + email + "|" + phone;
+        }
+    }
+}
